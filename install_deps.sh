47619642215755acadb0b876ca83f1f87acc05bb
MorganGeek <wattiez.morgan@gmail.com>
2017-11-21 21:36:20 +0100

    achieve setup and jenkins boot

---
+++ install_deps.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+sudo yum install ShellCheck --assumeyes
+sudo yum install java-1.8.0-openjdk --assumeyes
--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,3 +1,13 @@
-sudo wget --output-document /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo --no-check-certificate
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-sudo yum install jenkins-2.73.3 --assumeyes --nogpgcheck
+#!/usr/bin/env bash
+
+JENKINS_VERSION=2.73.3
+
+if ! yum list installed "jenkins" >/dev/null 2>&1; then
+   sudo wget --output-document /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo --no-check-certificate
+   sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+   sudo yum install jenkins-${JENKINS_VERSION} --assumeyes --nogpgcheck
+   sudo systemctl enable jenkins
+else
+   JENKINS_VERSION=$(yum info jenkins | grep Version | awk '{print $3}')
+   echo "Jenkins ${JENKINS_VERSION} is already installed"
+fi
